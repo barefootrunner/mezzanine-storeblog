06616d2d54a4931b7d535e8ffd9e796c18fe49a7
Your Name <you@example.com>
2019-06-02 23:08:22 -0500

    lesson 9 no images

--- mezzstoreblog/mezzstoreblog/settings.py
+++ mezzstoreblog/mezzstoreblog/settings.py
@@ -92,7 +92,7 @@ USE_MODELTRANSLATION = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -118,7 +118,7 @@ LANGUAGES = (
 # A boolean that turns on/off debug mode. When set to ``True``, stack traces
 # are displayed for error pages. Should always be set to ``False`` in
 # production. Best set to ``True`` in local_settings.py
-DEBUG = False
+DEBUG = True
 
 # Whether a user's session cookie expires when the Web browser is closed.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
@@ -345,4 +345,4 @@ else:
 ####################
 # CUSTOM SETTINGS #
 ####################
-BLOG_SLUG = ""
+BLOG_SLUG = "/"
